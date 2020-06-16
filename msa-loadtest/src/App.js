47bad11800b2e7d228c6219808ff6bd1d7061721
kimyujeong0605 <zmsgoaxhfl@naver.com>
2020-06-16 22:56:01 +0900

    시간컬럼

--- msa-loadtest/src/App.js
+++ msa-loadtest/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
 import Header from './components/common/Header';
 import TabList from './components/common/TabList';
+import './App.css';
+import customAxios from './customAxios';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  // IP주소 변수 선언
+  const [ip, setIp] = useState('');
+
+  // IP주소 값을 설정합니다.
+  function callback(data) {
+    setIp(data);
+  }
+
+  // 첫번째 렌더링을 다 마친 후 실행합니다.
+  useEffect(() => {
+    // 클라이언트의 IP주소를 알아내는 백엔드의 함수를 호출합니다.
+    customAxios('/ip', callback);
+  }, []);
+  console.log({ ip });
   return (
     <div>
       {' '}
-      <Header />
+      <Header />이 기기의 IP주소는 {ip}입니다.
       <TabList></TabList>
       {''}{' '}
     </div>
